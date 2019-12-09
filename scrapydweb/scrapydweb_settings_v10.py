ceacb8cae1e93b43eb00153d9e5d41bc8b8448c5
Zhang Di <zhangdi_me@163.com>
2019-12-09 16:20:30 +0800

    change scrapyd conf

--- scrapydweb/scrapydweb_settings_v10.py
+++ scrapydweb/scrapydweb_settings_v10.py
@@ -9,7 +9,6 @@ DOCS: https://github.com/my8100/files/blob/master/scrapydweb/README.md
 """
 import os
 
-
 ############################## QUICK SETUP start ##############################
 ############################## 快速设置 开始 ###################################
 # Setting SCRAPYDWEB_BIND to '0.0.0.0' or IP-OF-THE-CURRENT-HOST would make
@@ -24,7 +23,6 @@ ENABLE_AUTH = False
 # In order to enable basic auth, both USERNAME and PASSWORD should be non-empty strings.
 USERNAME = os.getenv('USERNAME')
 PASSWORD = os.getenv('PASSWORD')
-
 
 # Make sure that [Scrapyd](https://github.com/scrapy/scrapyd) has been installed
 # and started on all of your hosts.
@@ -46,11 +44,10 @@ PASSWORD = os.getenv('PASSWORD')
 #   - it's recommended to pass in a tuple of 5 elements.
 #   - e.g. ('', '', '127.0.0.1', '6800', '') or ('username', 'password', 'localhost', '6801', 'group')
 SCRAPYD_SERVERS = [
-    '127.0.0.1:6800',
+    os.getenv('SCRAPYD_HOST') + ':' + os.getenv('SCRAPYD_PORT'),
     # 'username:password@localhost:6801#group',
     ('username', 'password', 'localhost', '6801', 'group'),
 ]
-
 
 # It's recommended to update the three options below
 # if both ScrapydWeb and one of your Scrapyd servers run on the same machine.
@@ -74,10 +71,9 @@ LOCAL_SCRAPYD_LOGS_DIR = '/app/scrapyd/logs'
 # Note that you can run the LogParser service separately via command 'logparser' as you like.
 # Run 'logparser -h' to find out the config file of LogParser for more advanced settings.
 # Visit https://github.com/my8100/logparser for more info.
-ENABLE_LOGPARSER = True
+ENABLE_LOGPARSER = False
 ############################## QUICK SETUP end ################################
 ############################## 快速设置 结束 ###################################
-
 
 ############################## ScrapydWeb #####################################
 # The default is False, set it to True and add both CERTIFICATE_FILEPATH and PRIVATEKEY_FILEPATH
@@ -90,26 +86,22 @@ CERTIFICATE_FILEPATH = ''
 # e.g. '/home/username/cert.key'
 PRIVATEKEY_FILEPATH = ''
 
-
 ############################## Scrapy #########################################
 # ScrapydWeb is able to locate projects in the SCRAPY_PROJECTS_DIR,
 # so that you can simply select a project to deploy, instead of packaging it in advance.
 # e.g. 'C:/Users/username/myprojects' or '/home/username/myprojects'
 SCRAPY_PROJECTS_DIR = ''
 
-
 ############################## Scrapyd ########################################
 # ScrapydWeb would try every extension in sequence to locate the Scrapy logfile.
 # The default is ['.log', '.log.gz', '.txt'].
 SCRAPYD_LOG_EXTENSIONS = ['.log', '.log.gz', '.txt']
-
 
 ############################## LogParser ######################################
 # Whether to backup the stats json files locally after you visit the Stats page of a job
 # so that it is still accessible even if the original logfile has been deleted.
 # The default is True, set it to False to disable this behaviour.
 BACKUP_STATS_JSON_FILE = True
-
 
 ############################## Timer Tasks ####################################
 # Run ScrapydWeb with argument '-sw' or '--switch_scheduler_state', or click the ENABLED|DISABLED button
@@ -120,7 +112,6 @@ BACKUP_STATS_JSON_FILE = True
 # Note that this behaviour would be paused if the scheduler for timer tasks is disabled.
 # Set it to 0 to disable this behaviour.
 JOBS_SNAPSHOT_INTERVAL = 300
-
 
 ############################## Run Spider #####################################
 # The default is False, set it to True to automatically
@@ -152,7 +143,6 @@ SCHEDULE_DOWNLOAD_DELAY = None
 # Use '\r\n' as the line separator.
 SCHEDULE_ADDITIONAL = "-d setting=CLOSESPIDER_TIMEOUT=60\r\n-d setting=CLOSESPIDER_PAGECOUNT=10\r\n-d arg1=val1"
 
-
 ############################## Page Display ###################################
 # The default is True, set it to False to hide the Items page, as well as
 # the Items column in the Jobs page.
@@ -173,7 +163,6 @@ JOBS_RELOAD_INTERVAL = 300
 # which is displayed in the top right corner of the page.
 # The default is 10, set it to 0 to disable auto-refreshing.
 DAEMONSTATUS_REFRESH_INTERVAL = 10
-
 
 ############################## Send Text ######################################
 ########## usage in scrapy projects ##########
@@ -246,7 +235,6 @@ SMTP_PORT = 0
 SMTP_OVER_SSL = False
 # The timeout in seconds for the connection attempt, the default is 30.
 SMTP_CONNECTION_TIMEOUT = 30
-
 
 ############################## Monitor & Alert ################################
 # The default is False, set it to True to launch the poll subprocess to monitor your crawling jobs.
@@ -330,7 +318,6 @@ LOG_RETRY_TRIGGER_FORCESTOP = False
 LOG_IGNORE_THRESHOLD = 0
 LOG_IGNORE_TRIGGER_STOP = False
 LOG_IGNORE_TRIGGER_FORCESTOP = False
-
 
 ############################## System #########################################
 # The default is False, set it to True to enable debug mode and the interactive debugger
